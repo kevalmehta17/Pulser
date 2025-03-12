e0d6140b6d726210c0895e4a58dcc2c7fccc127b
Keval <kevalmehta885@gmail.com>
2025-03-12 20:53:31 +0530

    chore: Signin Controller builded

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,14 @@
 import { Context } from "hono";
 import { getPrisma } from "../config/prisma";
+import { decode, sign, verify } from 'hono/jwt'
+
+// Cloudflare Workers are stateless, so we must create a new Prisma client 
+// for each request to avoid connection pooling issues.
+//So that we have to call the getPrisma function to get a new Prisma client instance for each request.
+
 
 export const signIn = async (c: Context) => {
+
     const prisma = getPrisma(c.env.DATABASE_URL);
     const body = await c.req.json(); //Parse the json body
     try {
@@ -11,10 +18,22 @@ export const signIn = async (c: Context) => {
                 password: body.password,
             }
         });
+        if (user) {
+            const payload = {
+                id: user.id,
+                exp: Math.floor(Date.now() / 1000) + 60 * 5 //Token expires in 5 minutes
+            }
+            const secret = c.env.JWT_SECRET;
+            const token = await sign(payload, secret);
+            return c.json({ json: token });
+        }
         return c.text("signin success");
     } catch (error) {
         console.error(error);
-        return c.text("signin failed", 500);
+        if (error instanceof Error) {
+            return c.text(error.message);
+        }
+        return c.text("An unknown error occurred");
     }
 };
 
