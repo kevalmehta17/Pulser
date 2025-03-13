d2aa0100911f88b653149277e6786629f6c24f95
Keval <kevalmehta885@gmail.com>
2025-03-13 11:24:22 +0530

    Modified

--- src/config/prisma.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-export const getPrisma = (DATABASE_URL: string) => {
-    return new PrismaClient({
-        datasourceUrl: DATABASE_URL,
-    }).$extends(withAccelerate());
-};
--- src/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { login, signUp } from "../controllers/user.controller";
-
-const userRoutes = new Hono();
-
-userRoutes.post("/signup", signUp);
-userRoutes.post("/login", login);
-
-export default userRoutes;
