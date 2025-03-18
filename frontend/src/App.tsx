cd834a5660e394999f03e7191509aee23090b426
Keval <kevalmehta885@gmail.com>
2025-03-19 01:01:04 +0530

    fix: Modified code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
 import { Blog } from "./pages/Blog";
 import { Toaster } from "react-hot-toast";
+import { Blogs } from "./pages/Blogs";
 
 function App() {
+  const token = localStorage.getItem("token");
   return (
     <>
       <Routes>
+        <Route
+          path="/"
+          element={token ? <Navigate to="/blogs" /> : <Navigate to="/signup" />}
+        ></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/'blog/:id'" element={<Blog />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/blog/:id" element={<Blog />}></Route>
       </Routes>
       <Toaster />
     </>
